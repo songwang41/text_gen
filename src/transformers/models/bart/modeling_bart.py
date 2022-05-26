a8825d68da97103a1aeb8b56cd1dedd8a7d7f8ea
Song Wang <sonwang@microsoft.com>
2022-05-25 23:13:06 -0700

    fix bug in the adding cache

--- src/transformers/models/bart/modeling_bart.py
+++ src/transformers/models/bart/modeling_bart.py
@@ -1113,14 +1113,15 @@ class BartForConditionalGeneration(PretrainedBartModel):
 
     @staticmethod
     def _reorder_cache(past, beam_idx):
-        reordered_past = []
-        for layer_past in past:
-            # get the correct batch idx from decoder layer's batch dim for cross and self-attn
-            layer_past_new = {
-                attn_key: _reorder_buffer(attn_cache, beam_idx) for attn_key, attn_cache in layer_past.items()
-            }
-            reordered_past.append(layer_past_new)
-        return reordered_past
+        # reordered_past = []
+        # for layer_past in past:
+        #     # get the correct batch idx from decoder layer's batch dim for cross and self-attn
+        #     layer_past_new = {
+        #         attn_key: _reorder_buffer(attn_cache, beam_idx) for attn_key, attn_cache in layer_past.items()
+        #     }
+        #     reordered_past.append(layer_past_new)
+        # return reordered_past
+        return past.index_select(0, beam_idx)
 
     def get_encoder(self):
         return self.model.encoder
